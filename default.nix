48ef315c1a7d7ecbbf63074fd62d75e69b0bcd70
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-04-04 02:45:51 +0500

    chore: nix + reproducible builds

---
+++ default.nix
@@ -0,0 +1,54 @@
+{
+  pkgs ? let
+    lock = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked;
+    nixpkgs = fetchTarball {
+      url = "https://github.com/nixos/nixpkgs/archive/${lock.rev}.tar.gz";
+      sha256 = lock.narHash;
+    };
+  in
+    import nixpkgs {overlays = [];},
+  ...
+}: let
+  manifest = pkgs.lib.importJSON ./package.json;
+in
+  # pkgs.stdenv.mkDerivation {
+  pkgs.buildNpmPackage {
+    pname = manifest.name;
+    version = manifest.version;
+
+    src = ./.;
+    npmDepsHash = "sha256-i99qTy59VPJdQKB3E2Lm9AlNnUHL4+OV2zEXxM1ukUI=";
+
+    installPhase = ''
+      # Create output directory
+      mkdir -p $out
+
+      # Copy standalone as library
+      cp -R ./out/* $out/
+    '';
+
+    nativeBuildInputs = with pkgs; [
+      # Typescript
+      nodejs
+      pnpm
+      corepack
+
+      # Hail the Nix
+      nixd
+      statix
+      alejandra
+    ];
+
+    buildInputs = with pkgs; [
+      openssl
+      vips
+    ];
+
+    meta = with pkgs.lib; {
+      homepage = "https://trash.floss.uz";
+      description = manifest.description;
+      license = with licenses; [cc-by-40];
+      platforms = with platforms; linux ++ darwin;
+      maintainers = with maintainers; [orzklv];
+    };
+  }
